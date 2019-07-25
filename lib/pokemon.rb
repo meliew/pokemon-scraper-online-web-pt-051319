8d34e0b6f2a4e4e210a3ccf9897411b527b5db35
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-25 00:01:42 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  @@all = []
+
+  def initialize(id= nil, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    self.save(name, type, db)
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon(name, type) VALUES (?,?)", name, type)
+  end
+
+  def self.find(id, db)
+  find_pokemon = db.execute("SELECT *
+  FROM pokemon
+  WHERE id = ?
+  LIMIT 1")
+  end
 end
